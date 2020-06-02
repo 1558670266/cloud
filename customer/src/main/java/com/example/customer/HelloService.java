1deb1bd8bbdf1847891d91ef77ce68fcb9dcc8d9
wangzhiwei <wangzhwei@tydic.com>
2020-06-02 14:39:42 +0800

    'cloud工程实例'

---
+++ customer/src/main/java/com/example/customer/HelloService.java
@@ -0,0 +1,28 @@
+package com.example.customer;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author: WZW
+ * @date 2020/5/18 15:29
+ * @Description: TODO
+ */
+@Service
+public class HelloService {
+    @Autowired
+    RestTemplate restTemplate;
+    // 断路器配置，当无法调用如下方法时，就会调用自定的hiError方法。
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String hiService(String name) {
+        String str1 = restTemplate.getForObject("http://service-provider/hi?name=" + name, String.class);
+        return str1;
+    }
+
+    public String hiError(String name) {
+        return "hey " + name + ", there is some problem with hi page";
+    }
+
+}
---
+++ provider/src/main/java/com/example/provider/ProviderApplication.java
@@ -0,0 +1,34 @@
+package com.example.provider;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@EnableEurekaServer
+@EnableEurekaClient
+@RestController
+@EnableDiscoveryClient
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi " + name + ",i am from port:" + port;
+    }
+
+}
